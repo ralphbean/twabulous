d9b3de7f3f67ca60f4ca76792a35ddce8d7f2286
Ralph Bean <ralph.bean@gmail.com>
2011-11-21 23:17:48 -0500

    Files needed for pypi release #juggalos

---
+++ README.rst
@@ -0,0 +1,17 @@
+Twabulous
+=========
+
+Twitter + Fabulous = Twabulous
+
+Watch the twitter streaming sample api in fabulous color in your console!
+
+Install
+=======
+
+::
+        $ pip install twabulous
+
+Usage
+=====
+::
+        $ twabulous
---
+++ setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+f = open('README.rst')
+long_description = f.read().strip()
+f.close()
+
+setup(
+    name='twabulous',
+    version='9000.2a',
+    description="Twitter + Fabulous = Twabulous",
+    long_description=long_description,
+    author='Ralph Bean & Remy DeCausemaker',
+    author_email='ralph.bean@gmail.com',
+    url='http://github.com/ralphbean/twabulous/',
+    license='AGPLv3',
+    install_requires=[
+        "fabulous",
+        "simplejson",
+        "pycurl",
+        "PIL",
+    ],
+    packages=['twabulous'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    twabulous = twabulous.twabulous:main
+    """
+)
